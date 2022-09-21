29acca60654146756ada039571ad8d448002fc22
amandaalmeida20 <manda.fonsecaff@gmail.com>
2022-09-20 21:38:21 -0300

    Responsividade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -25,10 +25,10 @@ function App() {
  
  return (
  
- <div className="max-w-[1250px] mx-auto flex flex-col items-center my-20 flex-nowrap">
+ <div className="sm:max-w-[600px] lg:max-w-[1020px] xl:max-w-[1250px] max-w-[400px]  mx-auto flex flex-col items-center my-20 flex-nowrap">
     <img src={logoImg }alt="" />
 
-    <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span>  está aqui.</h1>
+    <h1 className=" text-3xl md:text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span>  está aqui.</h1>
 
     
         
--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -3,15 +3,17 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 export function CreateAdBanner(){
     return (
-        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-    <div className="bg-[#2A2634] px-8 py-6 rounded-t-lg flex justify-between items-center">
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg  mt-8 overflow-hidden">
+    <div className="bg-[#2A2634] px-5 py-3 sm:px-8 sm:py-6 rounded-t-lg flex justify-between items-center">
         <div>
-          <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-        <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>  
+          <strong className=" sm:text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+        <span className="text-zinc-400 block text-[10px] sm:text-[14px] lg:text-xl">Publique um anúncio para encontrar novos players!</span>  
         </div>
-        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24}/>
+        <Dialog.Trigger className=" py-1 px-2 sm:py-3 sm:px-3 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus  className="sm:w-5  sm:h-5 lg:w-7 lg:h-7"/>
+            <div className="text-[8px] sm:text-[12px] lg:text-2xl">
             Publicar anúncio
+            </div>
         </Dialog.Trigger>
     </div>
     </div>
