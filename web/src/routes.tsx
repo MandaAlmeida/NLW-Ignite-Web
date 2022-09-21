aaacac630e476f859ee35351ea7bb85adc9f4f76
amandaalmeida20 <manda.fonsecaff@gmail.com>
2022-09-21 00:41:07 -0300

    Create routes.tsx

---
+++ web/src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Game } from './Pages/Game';
+import { Home } from './Pages/Home';
+
+
+export function Nav(){
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+}
