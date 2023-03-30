57078a8d52b6235c1e0d5036ff15541184c68205
amanda almeida <manda.fonsecaff@gmail.com>
2023-03-30 02:29:13 -0300

    alterdando link do servidor

--- web/src/components/GameBannerMap.tsx
+++ web/src/components/GameBannerMap.tsx
@@ -1,61 +1,50 @@
-import {  useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import axios from 'axios';
-import { GameBanner } from './GameBanner';
-import  Slider  from './Slider';
-import {ReactElasticCarouselProps} from 'react-elastic-carousel';
-
-
-
+import axios from "axios";
+import { GameBanner } from "./GameBanner";
+import Slider from "./Slider";
+import { ReactElasticCarouselProps } from "react-elastic-carousel";
 
 interface Game {
-    id: string;
-    title: string;
-    bannerUrl: string;
-    _count: {
-        ads: number;
-    }
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
 }
 
+export function GamerBannerMap() {
+  const [games, setGames] = useState<Game[]>([]);
+  const settings: ReactElasticCarouselProps = {
+    isRTL: false,
+    itemPadding: [10, 8],
+    breakPoints: [
+      { width: 1, itemsToShow: 1 },
+      { width: 300, itemsToShow: 2, itemsToScroll: 2 },
+      { width: 600, itemsToShow: 3, itemsToScroll: 3 },
+      { width: 790, itemsToShow: 4, itemsToScroll: 4 },
+      { width: 1000, itemsToShow: 5, itemsToScroll: 5 },
+      { width: 1550, itemsToShow: 8, itemsToScroll: 8 },
+    ],
+  };
 
+  useEffect(() => {
+    axios("https://nlw-ignite-server.vercel.app/games").then((response) => {
+      setGames(response.data);
+    });
+  }, []);
 
-export function GamerBannerMap(){
-    const [games, setGames] = useState<Game[]>([])
-    const settings: ReactElasticCarouselProps =( 
-        {isRTL: false,
-        itemPadding: [10, 8],
-        breakPoints: [
-                { width: 1, itemsToShow: 1 },
-                { width: 300, itemsToShow: 2, itemsToScroll: 2 },
-                { width: 600, itemsToShow: 3, itemsToScroll: 3 },
-                { width: 790, itemsToShow: 4, itemsToScroll: 4 },
-                { width: 1000, itemsToShow: 5, itemsToScroll: 5 },
-                { width: 1550, itemsToShow: 8, itemsToScroll: 8 },
-              ],
-        
-      });
-
-    useEffect(() => {
-        axios('http://localhost:3333/games').then(response => {
-            setGames(response.data)
-        })
-        }, []) 
-
-    return (
-        <Slider settings={settings} >
-        {games.map(game => (
-          
-               <GameBanner 
-               key={game.id}
-               bannerUrl={game.bannerUrl}
-               title={game.title} 
-               adsCount={game._count.ads} 
-              
-               />
-               
-           )
-        )}
-   </Slider>
- 
-    )
+  return (
+    <Slider settings={settings}>
+      {games.map((game) => (
+        <GameBanner
+          key={game.id}
+          bannerUrl={game.bannerUrl}
+          title={game.title}
+          adsCount={game._count.ads}
+        />
+      ))}
+    </Slider>
+  );
 }
